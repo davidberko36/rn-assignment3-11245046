9bb1be042ac9746fe7030cf115fb7a949b51a53f
David Berko <daberko364@gmail.com>
2024-06-01 14:01:33 +0000

    Commit 1

--- assignment3/Components/Searchbar.js
+++ assignment3/Components/Searchbar.js
@@ -1,41 +1,57 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome'
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from "react-native";
 
-
-const SearchBar = ({placeholder}) => {
-    return (
-        <View style = {styles.container}>
-         <Icon name="search" size={16} color="#888" style={styles.searchIcon} />
-         <Text style = {styles.text}>{placeholder || 'Search'}</Text>
-         </View>
-    );
-};
-
-const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        borderRadius: 12,
-        marginVertical: 400,
-        marginTop: 0,
-        backgroundColor: 'white',
-        width: '75%',
-        height: 50,
-        paddingHorizontal: 10,
-    },
-
-    searchIcon: {
-        marginRight: 8,
-        paddingVertical: 10,
-        color: 'black',
-    },
-
-   text : {
-    fontSize: 16,
-    fontWeight: 'bold',
-    paddingVertical: 8
+export default function SearchBar() {
+  return (
+    <View style={styles.searchContainer}>
+        <View style={styles.inputContainer}>
+            <Image source={require("../assets/Vector.png")}/>
+            <TextInput style={styles.inputStyles} placeholder="Search" placeholderTextColor={"black"}/>
+        </View>
+        <TouchableOpacity style={styles.imgButtonStyles}>
+            <Image source={require("../assets/bx_slider.png")}/>
+        </TouchableOpacity>
+    </View>
+  );
 }
 
-})
+const styles = StyleSheet.create({
+   searchContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
+        marginVertical:25,
+        gap:20
+   },
+   inputContainer: {
+        backgroundColor: "white",
+        flexDirection: "row",
+        alignItems: "center",
+        borderRadius: 10,
+        padding:10,
+        gap:20,
+        width:"75%",
+        marginRight: 30,
+    },
+    imgButtonStyles: {
+        backgroundColor: "red",
+        padding:6,
+        borderRadius:10,
+        width: 50,
+    },
+    inputStyles: {
+        outline:undefined,
+        border:undefined
+    },
+});
 
-export default SearchBar;
+
+
+
+
+
+
+
+
+
+
